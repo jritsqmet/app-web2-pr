1190e65fe2ece042edf7858df6b1b3fe044476eb
unknown <jroserog@itsqmet.edu.ec>
2023-11-07 18:42:19 -0500

    Creación del componente nav-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NabVarComponent } from './components/nab-var/nab-var.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NabVarComponent
   ],
   imports: [
     BrowserModule,

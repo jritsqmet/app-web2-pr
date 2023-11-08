a8512365fff021ce397a1f35aad484b5adffec97
unknown <jroserog@itsqmet.edu.ec>
2023-11-07 19:03:11 -0500

    Creación de rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NabVarComponent } from './components/nab-var/nab-var.component';
+import { HomeComponent } from './screens/home/home.component';
+import { InfoComponent } from './screens/info/info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NabVarComponent
+    NabVarComponent,
+    HomeComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule,

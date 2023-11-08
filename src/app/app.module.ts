2e0836a339090b28fe7f2b7b523fde82f2259ccb
martinilguan12221@gmail.com <MartinIlg12@users.noreply.github.com>
2023-11-07 20:28:46 -0500

    Screen Martin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NabVarComponent } from './components/nab-var/nab-var.component';
 import { HomeComponent } from './screens/home/home.component';
 import { InfoComponent } from './screens/info/info.component';
+import { MartinComponent } from './screens/martin/martin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NabVarComponent,
     HomeComponent,
-    InfoComponent
+    InfoComponent,
+    MartinComponent
   ],
   imports: [
     BrowserModule,

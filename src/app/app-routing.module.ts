d223fafdf6fe19ec2942cdc7520f8a8ae73722ae
unknown <jroserog@itsqmet.edu.ec>
2023-11-07 19:10:44 -0500

    nueva ruta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { InfoComponent } from './screens/info/info.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
-  { path: 'info', component: InfoComponent}
+  { path: 'info', component: InfoComponent},
+  { path: '', component: HomeComponent}
 ];
 
 @NgModule({

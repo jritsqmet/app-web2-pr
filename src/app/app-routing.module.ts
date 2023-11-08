23159aad909320c193172f8b6659e85b85aaa030
martinilguan12221@gmail.com <MartinIlg12@users.noreply.github.com>
2023-11-07 20:36:02 -0500

    Tabla Martin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './screens/home/home.component';
 import { InfoComponent } from './screens/info/info.component';
+import { MartinComponent } from './screens/martin/martin.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
   { path: 'info', component: InfoComponent},
-  { path: '', component: HomeComponent}
+  { path: '', component: HomeComponent},
+  { path: 'martin', component: MartinComponent}
 ];
 
 @NgModule({
